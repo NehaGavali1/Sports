02d4b54c8516222673af5428d2ecd20faffad840
NehaGavali1 <nehagavali1@gmail.com>
2023-07-14 17:15:07 +0530

    done

---
+++ js/myscripts.js
@@ -0,0 +1,30 @@
+// Targeting video element 
+// let clip = document.querySelector(".vid")
+  
+/* Applying mouseover event on video clip 
+and then we call play() function to play 
+the video when the mouse is over the video */
+// clip.addEventListener("mouseover", function (e) {
+//     clip.play();
+// })
+
+/* Applying mouseout event on video clip 
+and then we call pause() function to stop 
+the video when the mouse is out the video */
+// clip.addEventListener("mouseout", function (e) {
+//     clip.pause();
+// })
+
+// $(document).ready(function(){
+//     $(function() {
+  
+//         var playersrc = jQuery(iframe).attr('src');
+  
+//             $('.vid').hover(function(){
+//             $('iframe').attr('src',playersrc+'&autoplay=1');
+//         }, function(){
+//             $('iframe').attr('src',playersrc);
+//         });
+//     });       
+//   });
+
